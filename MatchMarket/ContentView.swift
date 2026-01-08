7d4f31eaf6bd5f2777fa6b7e2cf009c0bd1db8b3
rama aili <rama.aili@education.nsw.gov.au>
2026-01-08 11:02:58 +1100

    Initial Commit

---
+++ MatchMarket/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MatchMarket
+//
+//  Created by Girl Geek on 8/1/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MatchMarket/MatchMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMarketApp.swift
+//  MatchMarket
+//
+//  Created by Girl Geek on 8/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct MatchMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
