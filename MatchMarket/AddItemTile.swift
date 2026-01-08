af28bc4f06291bd8e9b2e6e9c49677e8ab9de3c0
rama aili <rama.aili@education.nsw.gov.au>
2026-01-09 10:40:20 +1100

    added inbox + edits

--- MatchMarket/AddItemTile.swift
+++ MatchMarket/AddItemTile.swift
@@ -1,8 +1,23 @@
-//
-//  AddItemTile.swift
-//  MatchMarket
-//
-//  Created by Girl Geek on 8/1/2026.
-//
-
-import Foundation
+import SwiftUI
+struct AddItemTile: View {
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            VStack {
+                Image(systemName: "plus.circle.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 60)
+                    .foregroundColor(.blue)
+                
+                Text("Add Item")
+                    .font(.subheadline)
+                    .foregroundColor(.blue)
+            }
+            .frame(maxWidth: .infinity, minHeight: 140)
+            .background(Color(.systemGray6))
+            .cornerRadius(12)
+        }
+    }
+}
--- MatchMarket/StatTile.swift
+++ MatchMarket/StatTile.swift
@@ -1,8 +1,20 @@
-//
-//  StatTile.swift
-//  MatchMarket
-//
-//  Created by Girl Geek on 8/1/2026.
-//
-
-import Foundation
+import SwiftUI
+struct StatTile: View {
+    let title: String
+    let value: Int
+    
+    var body: some View {
+        VStack {
+            Text("\(value)")
+                .font(.title)
+                .fontWeight(.bold)
+            Text(title)
+                .font(.subheadline)
+                .foregroundColor(.gray)
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+        .background(Color(.systemGray6))
+        .cornerRadius(12)
+    }
+}
