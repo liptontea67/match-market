d97a8c7f2e08ea5daca65219fe070fabcb25df8d
rama aili <rama.aili@education.nsw.gov.au>
2026-01-08 11:15:24 +1100

    Added button

--- MatchMarket/ContentView.swift
+++ MatchMarket/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, SheHacks!")
+            Button("Press Me") {
+                
+            }
         }
         .padding()
     }
